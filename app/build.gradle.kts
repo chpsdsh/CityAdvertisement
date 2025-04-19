32a06b9741e9593da79f5e7c5f7915c92bf4cf0a
chpsdsh.fedora <bmxsib1@gmail.com>
2025-04-19 13:51:00 +0700

    registration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.nsu.cityadvertisement"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.nsu.cityadvertisement"
